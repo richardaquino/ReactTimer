2d80c6f109e3f885cb0b9c64d6c5f061c2d05ac5
Richard Aquino <richard.aquino@archibus-phil.com>
2020-03-03 21:15:29 +0800

    Add countdown functionality

--- app/tests/components/Coundown.test.jsx
+++ app/tests/components/Coundown.test.jsx
@@ -6,18 +6,33 @@ var TestUltils = require('react-addons-test-utils');
 
 var Countdown = require('Countdown');
 
-// describe('Countdown', () => {
-//     it('should-exist', () => {
-//         expect(Countdown).toExist();
-//     });
+describe('Countdown', () => {
+    it('should-exist', () => {
+        expect(Countdown).toExist();
+    });
 
-//     describe('handleSetCountdown', () => {
-//         it('should set state to be started and countdown', () => {
-//             var countdown = TestUltils.renderIntoDocument(<Countdown />);
-//             countdown.handleSetCountdown(10);
+    describe('handleSetCountdown', () => {
+        it('should set state to be started and countdown', (done) => {
+            var countdown = TestUltils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(10);
 
-//             expect(countdown.state.count).toBe(10);
-//             expect(countdown.state.countdownStatus).toBe('started');
-//         });
-//     });
-// });
+            expect(countdown.state.count).toBe(10);
+            expect(countdown.state.countdownStatus).toBe('started');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(9);
+                done();
+            }, 1001);
+        });
+
+        it('should never set count less than zero', (done) => {
+            var countdown = TestUltils.renderIntoDocument(<Countdown />);
+            countdown.handleSetCountdown(1);
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 3001);
+        });
+    });
+});
