1a8427f191b8ae6ed0e545a790b318ac5d322d11
Richard Aquino <richard.aquino@archibus-phil.com>
2020-03-05 09:34:44 +0800

    Update Files

---
+++ app/app.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
+
+//Load Foundation
+//require('style!css!foundation-sites/dist/css/foundation.min.css');
+require('style!css!foundation-sites/dist/css/foundation-float.min.css');
+$(document).foundation();
+
+//App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+   <Router history={hashHistory}>
+      <Route path="/" component={Main}>
+         <IndexRoute component={Timer}></IndexRoute>
+         <Route path="countdown" component={Countdown}></Route>
+      </Route>
+   </Router>,
+   document.getElementById('app')
+);
